b965807870a3894da3d24f9a17783c3461f1e592
Sergio <oliveira-sergio@outlook.com>
2026-08-19 22:52:41 +0100

    Update main.tf
    issue:
    # Error: waiting for EKS Node Group - Unhealthy nodes in the kubernetes cluster
    
    act:
    -> infra simplified to make easy identify the issue (not solved yet)
    -> EKS default
    -> private subnets elimintate (NAT Gateway too)

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,15 +23,19 @@ module "subnets_public" {
   name         = "mern"
 }
 
-module "subnets_private" {
-  source       = "./modules/subnets_private"
-  vpc_id       = module.vpc.id
-  cidr_block_a = "10.0.3.0/24"
-  cidr_block_b = "10.0.4.0/24"
-  az_a         = "eu-west-1a"
-  az_b         = "eu-west-1b"
-  name         = "mern"
-}
+# -----
+# Subnets
+# -----
+# EKS test
+# module "subnets_private" {
+#   source       = "./modules/subnets_private"
+#   vpc_id       = module.vpc.id
+#   cidr_block_a = "10.0.3.0/24"
+#   cidr_block_b = "10.0.4.0/24"
+#   az_a         = "eu-west-1a"
+#   az_b         = "eu-west-1b"
+#   name         = "mern"
+# }
 
 # -----
 # Internet Gateway
@@ -44,13 +48,14 @@ module "igw" {
 
 
 # -----
-# Internet Gateway
+# Nat Gateway
 # -----
-module "nat" {
-  source            = "./modules/nat"
-  public_subnet_id  = module.subnets_public.public_a_id
-  name              = "mern-nat"
-}
+# EKS test
+# module "nat" {
+#   source            = "./modules/nat"
+#   public_subnet_id  = module.subnets_public.public_a_id
+#   name              = "mern-nat"
+# }
 
 
 # -----
@@ -65,14 +70,15 @@ module "route_table_public" {
   name             = "mern"
 }
 
-module "route_table_private" {
-  source            = "./modules/route_table_private"
-  vpc_id            = module.vpc.id
-  nat_id            = module.nat.id
-  private_subnet_a  = module.subnets_private.private_a_id
-  private_subnet_b  = module.subnets_private.private_b_id
-  name              = "mern"
-}
+# EKS test
+# module "route_table_private" {
+#   source            = "./modules/route_table_private"
+#   vpc_id            = module.vpc.id
+#   nat_id            = module.nat.id
+#   private_subnet_a  = module.subnets_private.private_a_id
+#   private_subnet_b  = module.subnets_private.private_b_id
+#   name              = "mern"
+# }
 
 
 # -----
@@ -90,33 +96,15 @@ module "eks" {
 
   vpc_id = module.vpc.id
   subnet_ids = [
-    module.subnets_private.private_a_id,
-    module.subnets_private.private_b_id
+    module.subnets_public.public_a_id,
+    module.subnets_public.public_b_id
   ]
 
   eks_managed_node_groups = {
     default = {
-      instance_types = ["t3.micro"]
-      desired_size   = 2
-      max_size       = 3
-      min_size       = 1
-
-      iam_role_additional_policies = {
-        "AmazonEKSWorkerNodePolicy"          = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-        "AmazonEC2ContainerRegistryReadOnly" = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-        "AmazonEKS_CNI_Policy"               = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-        "AmazonSSMManagedInstanceCore"       = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-      }
-
-      tags = {
-        "Name"        = "mern-eks-node-default"
-        "Environment" = "dev"
-      }
+      desired_size = 2
+      min_size     = 1
+      max_size     = 3
     }
-  }
-
-  tags = {
-    "Name"        = "mern-eks"
-    "Environment" = "dev"
   }
 }
