05945056fb44af1a006192a152a415790fc94081
Sergio <oliveira-sergio@outlook.com>
2026-08-30 22:28:36 +0100

    Update server.js
    act:
    -> health check created

--- mern/server/server.js
+++ mern/server/server.js
@@ -1,12 +1,30 @@
 import express from "express";
 import cors from "cors";
 import records from "./routes/record.js";
+import db from "./db/connection.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+app.get("/health", async (req, res) => {
+  try {
+    await db.command({ping: 1});
+    res.status(200).json({
+      status: "UP",
+      database: "UP"
+    });
+  } catch (error){
+    res.status(503).json({
+      status: "DOWN",
+      database: "DOWN"
+    });
+  }
+})
+
 app.use("/record", records);
 
 // start the Express server
