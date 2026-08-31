bb35ee96a6d66045564ebac9eae8caccfeb7c91b
Sergio <oliveira-sergio@outlook.com>
2026-08-31 15:38:20 +0100

    Update server.js
    health check was split in two different responsibilities.
    ready and live
    
    act:
    -> endpoint "ready" defined

--- mern/server/server.js
+++ mern/server/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-app.get("/health", async (req, res) => {
+app.get("/health/ready", async (req, res) => {
   const databaseHealthy = await checkDatabaseHealth();
 
   if (databaseHealthy) {
